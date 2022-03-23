149c414a47898f1828b98e48f7154c9a663d0654
Meryem Aydin <meryemaydinsener@gmail.com>
2022-03-23 09:20:57 +0100

    Update For_While.java

--- src/replit_English/For_While.java
+++ src/replit_English/For_While.java
@@ -11,5 +11,21 @@ char ch1= 'a' ;
 String name =“John came late"
 
 Expected Output: Number of a = 2*/
+        
+         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the name");
+
+        String name = sc.next();
+        System.out.println("Enter a letter");
+        char harf = sc.next().charAt(0);
+
+        int sum = 0;
+        for (int i = 0; i < name.length(); i++) {
+            if (harf == name.charAt(i)) {
+                sum++;
+            }
+
+        }
+        System.out.println(sum);
     }
 }
